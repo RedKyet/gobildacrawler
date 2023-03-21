c18415dd9f346fe5d7528d6d59f23a647c3d9917
Teodor Moscaliuc <teodormoscaliuc@outlook.com>
2023-03-21 21:30:39 +0200

    unzipping

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import requests
+import zipfile
+
 with open("tmp.zip",'wb') as f:
     f.write(requests.get("https://www.gobilda.com/content/step_files/2000-0025-0002.zip").content)
+
+with zipfile.ZipFile('tmp.zip', 'r') as zip_ref:
+    zip_ref.extractall()
